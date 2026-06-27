40aec09a12e8a83619dd6093368b1d84bab22a4e
MayureshDandekar <mayureshdandekar.dev@gmail.com>
2026-06-27 16:10:05 +0530

    Stage 2: Add speech Recognition with takeCommand function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr  
 
 engine = pyttsx3.init()
 
@@ -6,4 +7,32 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
 
-speak("Initializing Jarvis just a second")
+speak("Initializing Jarvis")
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        r.adjust_for_ambient_noise(source, duration=1)
+        print("Listneing...")
+        r.pauseThreshold = 1
+
+        try:
+            audio = r.listen(source, timeout=5, phrase_time_limit=5)
+            print("Recognising...")
+            query = r.recognize_google(audio, language="en-in")
+            print(f"You said {query}")
+            return query
+        except Exception as e:
+            print("Could not understand, please say again")
+            return "none"
+
+takeCommand()
+
+
+
+
+
+
+
+
